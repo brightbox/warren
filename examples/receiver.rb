ba9818aa1de0786e0f3f105c59562051bf100d6e
Caius Durling <dev@caius.name>
2009-02-10 01:24:09 +0000

    Tidying up examples

--- examples/receiver.rb
+++ examples/receiver.rb
@@ -1,15 +1,17 @@
 require "rubygems"
-require "../warren"
+require File.expand_path(File.dirname(__FILE__) + "/../warren")
 
 Signal.trap("INT") { AMQP.stop { EM.stop } }
 Signal.trap("TERM") { AMQP.stop { EM.stop } }
 
-q = "reboots"
-
+# Listen to the main queue
+q = "main"
 puts "Listening to the #{q} queue."
 
-Warren::Queue.set_connection(:user => "network", :pass => "network", :vhost => "taskomatic")
+# Setup the connection directly this time
+Warren::Queue.set_connection(:user => "caius", :pass => "caius", :vhost => "/")
 
+# And attach a block for new messages to fire
 Warren::Queue.subscribe(q) do |msg|
   p [Time.now, msg]
 end
--- examples/sender.rb
+++ examples/sender.rb
@@ -1,15 +1,30 @@
 require "rubygems"
-require "../warren"
+require File.expand_path(File.dirname(__FILE__) + "/../warren")
 
 Signal.trap("INT") { AMQP.stop { EM.stop } }
 Signal.trap("TERM") { AMQP.stop { EM.stop } }
 
-# Setup our own connection for no good reason
+# Setup our own connection before generating the queue object
 conn = Warren::Connection.new(
-  :user => "network",
-  :pass => "network",
-  :vhost => "taskomatic",
-  :default_queue => "reboots"
+  :user => "caius",
+  :pass => "caius",
+  :vhost => "/",
+  :default_queue => "main"
 )
+# Set the connection for the queue
 Warren::Queue.set_connection(conn)
-Warren::Queue.publish(:default, {:name => "fred"})
+# Generate some data to send
+data = {
+  :people => [
+    :fred => {
+      :age => 25,
+      :location => "Leeds"
+    },
+    :george => {
+      :age => 32,
+      :location => "London"
+    }
+  ]
+}
+# And then push a message onto the queue
+Warren::Queue.publish("main", data)
