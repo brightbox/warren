47efef4054fb8b8dc8e2ef3b828173b3cecc1318
Caius Durling <dev@caius.name>
2009-02-12 16:22:41 +0000

    Updating Warren::Queue so it sends more than one message from the same process

--- lib/queue.rb
+++ lib/queue.rb
@@ -6,12 +6,20 @@ class Warren::Queue
     @@connection = params.is_a?(Warren::Connection) ? params : Warren::Connection.new(params)
   end
   
-  # Sends a payload to a queue
+  #
+  # Sends a payload to a queue. If successfully sent it returns
+  # true, unless callback block is passed (see below)
   # 
-  # eg:
   #   Warren::Queue.publish(:queue_name, {:foo => "name"})
   #   Warren::Queue.publish(:queue_name, #<Warren::Message>)
-  #
+  # 
+  # Can also pass a block which is fired after the message
+  # is sent. If a block is passed, then the return value of the block
+  # is returned from this method.
+  # 
+  #   Warren::Queue.publish(:queue_name, {:foo => "name"}  ) { puts "foo" }
+  #   Warren::Queue.publish(:queue_name, #<Warren::Message>) { puts "bar" }
+  # 
   def self.publish queue_name, payload, &blk
     if queue_name == :default
       queue_name = @@connection.queue_name
@@ -21,21 +29,22 @@ class Warren::Queue
     msg ||= payload
     
     do_connect(true, blk) do
-      queue = MQ.queue queue_name
+      queue = MQ::Queue.new(MQ.new, queue_name)
       queue.publish msg.to_s
     end
 
   end
   
+  #
   # Subscribes to a queue and runs the block
   # for each message received
   # 
-  # eg:
   #   Warren::Queue.subscribe("example") {|msg| puts msg }
+  # 
   def self.subscribe queue_name, &block
     # todo: check if its a valid queue?
     do_connect(false) do
-      queue = MQ.queue queue_name
+      queue = MQ::Queue.new(MQ.new, queue_name)
       queue.subscribe do |msg|
         msg = Warren::Message.unpack(msg)
         block.call(msg)
@@ -48,11 +57,12 @@ class Warren::Queue
   
   # Connects and does the stuff its told to!
   def self.do_connect should_stop = true, callback = nil, &block
-    AMQP.start(@@connection.options) do 
+    AMQP.start(@@connection.options) do
       block.call
-      AMQP.stop { EM.stop } if should_stop
+      AMQP.stop { EM.stop_event_loop } if should_stop
     end
-    callback.call unless callback.nil?
+    # Returns the block return value or true
+    callback.nil? ? true : callback.call
   end
-
+  
 end
