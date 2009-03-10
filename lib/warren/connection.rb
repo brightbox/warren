abfb2b82e5cafb3440467044e4d85ff51f3cb440
Caius Durling <dev@caius.name>
2009-03-10 15:38:30 +0000

    Removing uneeded property accessor

--- lib/warren/connection.rb
+++ lib/warren/connection.rb
@@ -1,7 +1,5 @@
 class Warren::Connection
-  
-  attr_reader :opts
-  
+    
   def initialize opts = {}
     # Check they've passed in the stuff without a default on it
     unless opts.has_key?(:user) && opts.has_key?(:pass) && opts.has_key?(:vhost)
