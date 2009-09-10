bbee73f88c5f566bd730e6ac8cf43fcc7c1aca51
Caius Durling <dev@caius.name>
2009-09-10 15:38:29 +0100

    Changing the dummy adapter to use Warren::Queue.logger

--- lib/warren/adapters/dummy_adapter.rb
+++ lib/warren/adapters/dummy_adapter.rb
@@ -1,12 +1,12 @@
 require "rubygems"
 
 class DummyAdapter < Warren::Queue
+
   def self.publish queue_name, payload, &blk
-    puts "publishing #{payload.inspect} to #{queue_name}"
+    logger.info "publishing #{payload.inspect} to #{queue_name}"
   end
 
   def self.subscribe queue_name, &block
-    puts "subscribing to #{queue_name}"
+    logger.info "subscribing to #{queue_name}"
   end
 end
-
